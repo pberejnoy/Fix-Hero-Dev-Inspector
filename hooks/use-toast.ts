f7ea3a881839df1dc14c695cd3c4f5b17b09dbd6
pberejnoy <pberejnoy@gmail.com>
2025-04-12 17:28:52 -0700

    Add files via upload

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -7,7 +7,7 @@ import type {
   ToastActionElement,
   ToastProps,
 } from "@/components/ui/toast"
-/* import { ToastActionElement, ToastProps } from "@/components/ui/toast"*/
+
 const TOAST_LIMIT = 1
 const TOAST_REMOVE_DELAY = 1000000
 
